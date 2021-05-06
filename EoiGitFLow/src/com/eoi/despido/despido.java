6c2c0433793f290e5aefaca7100b6c476be90f93
Nacho <nachomarzar@gmail.com>
2021-05-06 19:41:46 +0200

    Mi segundo commit en java

---
+++ EoiGitFLow/src/com/eoi/despido/despido.java
@@ -0,0 +1,11 @@
+package com.eoi.despido;
+
+public class despido {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Adios");
+
+	}
+
+}
--- EoiGitFLow/src/com/eoi/saludo/Saludo.java
+++ EoiGitFLow/src/com/eoi/saludo/Saludo.java
@@ -3,7 +3,8 @@ package com.eoi.saludo;
 public class Saludo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hola que tal");
 
 	}
 
